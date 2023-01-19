06a7d22cab9dd0465e4544549b29f8af48de4fd3
bfioh <smicov01@inbox.ru>
2023-01-20 00:53:07 +0500

    timus

--- src/timus/task_2012/task_2012.java
+++ src/timus/task_2012/task_2012.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class task_2012 {
     public static void main(String[] args) {
-//        System.out.println("Введите количество задач от 1 до 11");
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
         if (a>=7 && a<=11){
