dafd53ec50f2cfa86e2d351be8937dcdb152b5dd
bfioh <smicov01@inbox.ru>
2023-05-01 16:08:19 +0500

    lr2

--- src/lr2/Example_2.java
+++ src/lr2/Example_2.java
@@ -1,7 +1,5 @@
 package lr2;
-
 import java.util.Scanner;
-
 public class Example_2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -24,7 +22,4 @@ public class Example_2 {
             System.out.println("Введённое число не делится на 7 с остатком 1");
         }
         }
-
-
-
     }
